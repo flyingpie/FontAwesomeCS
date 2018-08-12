678156679fc0883bd6c35b74981735ecbdf583b2
Marco van den Oever <marco@flyingpie.nl>
2018-08-12 14:06:00 +0200

    Extensions - Key(), Label(), Style(), StyleString() and Unicode()

--- FontAwesomeCS/Extensions.cs
+++ FontAwesomeCS/Extensions.cs
@@ -5,8 +5,44 @@
 
     public static class Extensions
     {
+        /// <summary>
+        /// Returns the key of the icon, eg "github-alt"
+        /// </summary>
+        public static string Key(this FAIcon icon) => icon.GetAttributeOfType<FAIconAttribute>().Key;
+
+        /// <summary>
+        /// Returns the label of the icon, eg "Alternate GitHub"
+        /// </summary>
+        public static string Label(this FAIcon icon) => icon.GetAttributeOfType<FAIconAttribute>().Label;
+
+        /// <summary>
+        /// Returns the unicode character of the icon, eg "f113"
+        /// </summary>
+        public static string Unicode(this FAIcon icon) => icon.GetAttributeOfType<FAIconAttribute>().Unicode;
+
+        /// <summary>
+        /// Returns the style enum of the icon, eg <see cref="FAStyle.Brands" />
+        /// </summary>
+        public static FAStyle Style(this FAIcon icon) => icon.GetAttributeOfType<FAIconAttribute>().Style;
+
+        /// <summary>
+        /// Returns the style of the icon as a string, eg "brands"
+        /// </summary>
+        public static string StyleString(this FAIcon icon) => icon.GetAttributeOfType<FAIconAttribute>().Style.Style();
+
+        /// <summary>
+        /// Returns the name of the style, eg "brands"
+        /// </summary>
+        public static string Style(this FAStyle icon) => icon.GetAttributeOfType<FAStyleAttribute>().Style;
+
+        /// <summary>
+        /// Returns the <see cref="FAIconAttribute" /> associated with the icon, containing its metadata
+        /// </summary>
         public static FAIconAttribute GetFAIconAttribute(this FAIcon icon) => icon.GetAttributeOfType<FAIconAttribute>();
 
+        /// <summary>
+        /// Returns the <see cref="FAStyleAttribute" /> associated with the style, containing its metadata
+        /// </summary>
         public static FAStyleAttribute GetFAStyleAttribute(this FAStyle style) => style.GetAttributeOfType<FAStyleAttribute>();
 
         private static T GetAttributeOfType<T>(this Enum enumVal) where T : Attribute
